b69d4ffb97dee98e53e3b283ed0bf766705a233c
hasanpoor1 <hasanpormhmdjavad@gmail.com>
2025-04-13 17:57:53 +0330

    Add files via upload

---
+++ project222.py
@@ -0,0 +1,25 @@
+print("عملگر های جمع ضرب و...:")
+print(10 + 20)
+print(20 - 10)
+print(2 * 8)
+print(2 / 6)
+print(9 ** 3)
+print(21 // 7)
+print(20 % 2)
+#-----------------  == > <
+print("عملگر های == >  < و ... :")
+print(2.1 == 4.1)  ;  print(256 == 256)
+print(256 != 256)  ; print(256 != 2578)
+print(255 < 255.1) ;  print(255 < 255)
+print(990 > 900)   ;  print(255 > 300)
+print(255 <= 255)  ; print(255 <= 255)
+print(550 >= 550)  ;  print(551 >= 341)
+#------------------------------------ and or not
+print("عملگر های not or and و ... :")
+print(2.1 == 4.1 and 20 == 20)  ;  print( 250 == 250 and 1000 == 1000)
+print(2.1 == 4.1 or 2.1 == 4.1 )  ;  print(251 == 251 or  990 != 983)
+F = False
+print(not F)
+T = True
+print(not T)
+
